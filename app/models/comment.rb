8993d554c01aa6d2e40236a3cc09c9376bff5ef8
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2022-06-08 13:58:33 +0600

    Posts: fixes in dependency of comment reply and all post and your post pages

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  has_many :comment_replies
+  has_many :comment_replies, dependent: :destroy
 
   validates :commenter, :body, presence: true
 end
