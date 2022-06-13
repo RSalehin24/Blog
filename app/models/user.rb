75cbfe29c2dd6ec9b84179cb62f7b24ec781b7c2
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2022-06-13 21:50:07 +0600

    Model: User:  :email-presence is validated now

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
          
          has_many :posts, dependent: :destroy
-         validates :first_name, :last_name, :username, presence: true
+         validates :first_name, :last_name, :username, :email, presence: true
          validates :username, uniqueness: true
 
          has_one_attached :image, dependent: :destroy
