40cbf4c9c1d1eec182d0dbaa55ee3c01df1fdc86
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2022-06-13 16:32:39 +0600

    Migration: edited migration to keep the database generation consistency

--- db/migrate/20220613085830_remove_category_from_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCategoryFromPosts < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :posts, :category, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_085830) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_13_102853) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
