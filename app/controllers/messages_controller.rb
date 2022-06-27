02e5adef97b253af9bc939f86cf8c6bfb6f5f1cf
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2022-06-27 15:48:47 +0600

    Revert "Message-attachment:"
    This reverts commit ec4963b85ab96d193d4e45ecd99227bbedbb85fc.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,15 +2,11 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
   
   def create
-    @message = current_user.messages.create(
-      body: msg_params[:body], 
-      room_id: params[:room_id]
-      attachments: msg_params[:attachments]
-    )
+    @message = current_user.messages.create(body: msg_params[:body], room_id: params[:room_id])
   end
 
-  private
+
   def msg_params
-    params.require(:message).permit(:body, attachments: [])
+    params.require(:message).permit(:body)
   end
 end
